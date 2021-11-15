60b70dfd00073c74df632c98ce0f8d2230382062
AGBullet <denism3melnikov@list.ru>
2021-11-15 22:56:51 +0300

    HW spring

---
+++ src/main/java/com/homework/spring/web/controllers/MainController.java
@@ -0,0 +1,34 @@
+package com.homework.spring.web.controllers;
+
+import com.homework.spring.web.model.Product;
+import com.homework.spring.web.repo.ProductRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    private ProductRepo productRepo;
+    public MainController(ProductRepo productRepo) {
+        this.productRepo = productRepo;
+    }
+
+
+
+
+    @GetMapping("/products")
+    public String showProductPage(Model model) {
+        model.addAttribute("products", productRepo.getAllProducts());
+        return "products_page";
+    }
+
+    @GetMapping("/products/{id}")
+    public String showProductPage(Model model, @PathVariable Long id) {
+       Product product = productRepo.findById(id);
+        model.addAttribute("product", product);
+        return "products_info_page";
+    }
+}
---
+++ src/main/java/com/homework/spring/web/repo/ProductRepo.java
@@ -0,0 +1,38 @@
+package com.homework.spring.web.repo;
+
+import com.homework.spring.web.model.Product;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class ProductRepo {
+    private List<Product> products;
+
+    @PostConstruct
+    public void init() {
+        products = new ArrayList<>(List.of(
+                new Product(1L, "Milk",10L),
+                new Product(2L, "Bread",100L),
+                new Product(3L, "Surge",102L),
+                new Product(4L, "Bear",103L),
+                new Product(5L, "Vino",104L)
+
+        ));
+
+
+
+    }
+    public List<Product> getAllProducts() {
+        return Collections.unmodifiableList(products);
+    }
+    
+    public Product findById(Long id) {
+        return products.stream().filter(s -> s.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+}
